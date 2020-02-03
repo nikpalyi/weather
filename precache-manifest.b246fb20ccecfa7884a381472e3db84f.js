36c81f5b1ea99afe839a35d856a529c031dabe7f
Nikolett Palyi <nikpalyi@gmail.com>
2020-02-03 15:26:46 +0000

    Updates

---
+++ precache-manifest.b246fb20ccecfa7884a381472e3db84f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebbc1beadd6df4185dee0c6c425b956",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "253fcdae37968b0448bd",
+    "url": "/weather/static/css/main.33fa6bf7.chunk.css"
+  },
+  {
+    "revision": "6e3e1ea8a1703768ad7b",
+    "url": "/weather/static/js/2.f79569f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.f79569f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253fcdae37968b0448bd",
+    "url": "/weather/static/js/main.e476b3ad.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  }
+]);
